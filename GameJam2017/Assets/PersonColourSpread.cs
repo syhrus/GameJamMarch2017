7ee5c95668a72065ece3364002774e7fda688617
syhrus <tugman1234@hotmail.com>
2017-03-05 16:15:04 +1100

    People and passive idea spread
    -Added Person Gameobject
    -Added Colour control scripts
    -Added Colour spread scripts
    
    -Added Test Scene to view changes.

---
+++ GameJam2017/Assets/PersonColourSpread.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonColourSpread : MonoBehaviour {
+
+    public float vocalRadius = 0; //Radius of trigger
+    public float changepower = 0.01f; //Amount of change per cycle
+    public float changeRate = 10; //Tweaker for rate of change
+
+    private List<PersonColourControl> inRadius;
+    private PersonColourControl thisColours; //To get current colour
+
+	void Start () {
+        inRadius = new List<PersonColourControl>();
+        thisColours = GetComponent<PersonColourControl>();
+
+        StartCoroutine(SpreadColour());
+    }
+
+    //Running as coroutine to save performance
+    IEnumerator SpreadColour()
+    {
+        while (true) //TODO: Add global "is game running?" variable to check off
+        {
+            //Finds all colliders in a radius and adds them to list
+            foreach( Collider2D p in Physics2D.OverlapCircleAll(transform.position, vocalRadius))
+            {
+                inRadius.Add(p.transform.GetComponent<PersonColourControl>());
+            }
+            //For each person in range, add colours to them
+            for(int i = 0; i < inRadius.Count; i++)
+            {
+                PersonColourControl person = inRadius[i];
+                person.blue += thisColours.blue * changepower;
+                person.red += thisColours.red * changepower;
+            }
+
+            yield return new WaitForSeconds(changeRate);
+            inRadius.Clear();
+        }
+    }
+}
---
+++ GameJam2017/Assets/Scripts/PersonColourControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonColourControl : MonoBehaviour {
+
+    //Inner Colours
+    public float blue = 0;
+    public float red = 0;
+    private float grey = 0.5f;
+
+    //Border Colours
+    public float borderBlue = 0; 
+    public float borderRed = 0;
+
+    private SpriteRenderer border; //Border Sprite of person
+    private SpriteRenderer inner; //Inner Sprite of person
+
+	void Start () {
+        //Get border and inner sprites.
+        border = transform.FindChild("Border").GetComponent<SpriteRenderer>();
+        inner = transform.FindChild("Inner").GetComponent<SpriteRenderer>();
+    }
+	
+	void Update () {
+        //clamp colour values so they don't get too large - This also adds a little bit of a decay factor, so that's nice.
+        while (red + blue > 1)
+        {
+            red /= 2;
+            blue /= 2;
+        }
+        grey = 0.5f - red - blue;
+
+        borderBlue = Mathf.Clamp01(borderBlue);
+        borderRed = Mathf.Clamp01(borderRed);
+
+        //set inner colour 
+        inner.color = new Color(
+               grey + red,
+               grey,
+               grey + blue
+               );
+        //set border colour 
+        border.color = new Color(
+               0.0f + (borderRed),
+               0.0f,
+               0.0f + (borderBlue)
+               );
+    }
+}
