9dba08170dd542064343a36b5ef8de50d1eee88f
ctoabn <nicholas.gath@gmail.com>
2017-03-06 18:50:50 +1100

    Movement
    Added PersonMovement and FriendSeek

---
+++ GameJam2017/Assets/Scripts/FriendSeek.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendSeek : MonoBehaviour {
+
+	public float vocalRadius = 0;
+	private List<PersonColourControl> inRadius;
+	private Rigidbody2D me;
+	private PersonMovement personMoving;
+
+	// Use this for initialization
+	void Start () {
+		inRadius = new List<PersonColourControl>();
+		me = GetComponent<Rigidbody2D> ();
+		personMoving = GetComponent<PersonMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Finds all colliders in a radius and adds them to list
+		foreach( Collider2D p in Physics2D.OverlapCircleAll(transform.position, vocalRadius))
+		{
+			inRadius.Add(p.transform.GetComponent<PersonColourControl>());
+		}
+		//For each person in range, add colours to them
+		for(int i = 0; i < inRadius.Count; i++)
+		{
+			Vector2 desiredVel = (inRadius [i].transform.position - transform.position).normalized * personMoving.maxSpeed;
+			personMoving.dir+=(desiredVel - me.velocity);
+		}
+
+
+		inRadius.Clear();
+		
+	}
+}
---
+++ GameJam2017/Assets/Scripts/PersonMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonMovement : MonoBehaviour {
+
+	public float maxSpeed = 10; //adjustable speed
+	public Vector2 dir;
+	private Rigidbody2D me;
+
+	// Use this for initialization
+	void Start () {
+		me = GetComponent<Rigidbody2D> ();
+		dir = Vector2.zero;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		me.AddForce(dir);
+	}
+}
