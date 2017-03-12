f813f558be7d883aa436445d93f0d447614c8d9b
syhrus <tugman1234@hotmail.com>
2017-03-13 01:21:14 +1100

    Flock behaviour
    The sheep flock like... sheep.

--- GameJam2017/Assets/Scripts/FriendSeek.cs
+++ GameJam2017/Assets/Scripts/FriendSeek.cs
@@ -6,6 +6,7 @@ public class FriendSeek : MonoBehaviour {
 
 	public float seekRadius = 0;
 	private List<PersonColourControl> inRadius;
+    private List<Transform> nearBorders;
 	private PersonColourControl meColour;
 	private Rigidbody2D meBody;
 	private PersonMovement personMoving;
@@ -13,7 +14,10 @@ public class FriendSeek : MonoBehaviour {
 	private int targetColour = 0;
 
     public float friendWeight = 1;
+    public float sameWeight = 1;
     public float enemyWeight = 1;
+    public float borderWeight = 1;
+    public float flockWeight = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -21,57 +25,96 @@ public class FriendSeek : MonoBehaviour {
 		meBody = GetComponent<Rigidbody2D> ();
 		meColour = GetComponent<PersonColourControl> ();
 		personMoving = GetComponent<PersonMovement>();
-        
+        nearBorders = new List<Transform>();
+        StartCoroutine(SeekFriends());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//checks my colour
-		if (meColour.red > meColour.green) {
-			myColour = 1;
-		} else {
-			myColour = 2;
-		}
-		//Finds all colliders in a radius and adds them to list
-		foreach( Collider2D p in Physics2D.OverlapCircleAll(transform.position, seekRadius))
-		{
-            if (p.tag == "Person")
+
+    // Update is called once per frame
+    IEnumerator SeekFriends()
+    {
+        while (true)
+        {
+            //checks my colour
+            if (meColour.red > meColour.green)
             {
-                inRadius.Add(p.transform.GetComponent<PersonColourControl>());
+                myColour = 1;
             }
-		}
-		//For each person in range, add colours to them
-		for(int i = 0; i < inRadius.Count; i++)
-		{
-			//checks targets colour
-			if (inRadius[i].red >= inRadius[i].green) {
-				targetColour = 1;
-			} else {
-				targetColour = 2;
-			}
-
-			if (meColour.myFriends == inRadius [i].myFriends) {
-				if (targetColour != myColour) {//we are different colours
-
-					Vector2 desiredVel = (inRadius [i].transform.position - transform.position).normalized * personMoving.maxSpeed;
-					personMoving.dir -= (desiredVel - meBody.velocity);
-
-				} else {//else we are similar colours
-
-					Vector2 desiredVel = (inRadius [i].transform.position - transform.position).normalized * personMoving.maxSpeed;
-					personMoving.dir += (desiredVel - meBody.velocity);
-
-				}
-			} else {
-
-				Vector2 desiredVel = (inRadius [i].transform.position - transform.position).normalized * personMoving.maxSpeed;
-				personMoving.dir -= (desiredVel - meBody.velocity);
-
-			}
-		}
+            else
+            {
+                myColour = 2;
+            }
+            //Finds all colliders in a radius and adds them to list
+            foreach (Collider2D p in Physics2D.OverlapCircleAll(transform.position, seekRadius))
+            {
+                if (p.tag == "Person")
+                {
+                    inRadius.Add(p.transform.GetComponent<PersonColourControl>());
+                }
+                if (p.tag == "Borders")
+                {
+                    nearBorders.Add(p.transform);
+                }
+            }
+            //For each person in range, add colours to them
+            for (int i = 0; i < inRadius.Count; i++)
+            {
+                //checks targets colour
+                if (inRadius[i].red >= inRadius[i].green)
+                {
+                    targetColour = 1;
+                }
+                else
+                {
+                    targetColour = 2;
+                }
 
 
-		inRadius.Clear();
-		
-	}
+                if (meColour.myFriends == inRadius[i].myFriends)
+                {
+                    if (targetColour != myColour)
+                    {//we are different colours
+
+                        Vector2 desiredVel = (inRadius[i].transform.position - transform.position).normalized * personMoving.maxSpeed;
+                        personMoving.dir += (desiredVel - meBody.velocity) * friendWeight * enemyWeight;
+
+                    }
+                    else
+                    {//else we are similar colours
+
+                        Vector2 desiredVel = (inRadius[i].transform.position - transform.position).normalized * personMoving.maxSpeed;
+                        personMoving.dir += (desiredVel - meBody.velocity) * friendWeight * sameWeight;
+
+                    }
+                }
+                if (targetColour != myColour)
+                {//we are different colours
+
+                    Vector2 desiredVel = (inRadius[i].transform.position - transform.position).normalized * personMoving.maxSpeed;
+                    personMoving.dir -= (desiredVel - meBody.velocity) * enemyWeight;
+
+                }
+                else
+                {//else we are similar colours
+
+                    Vector2 desiredVel = (inRadius[i].transform.position - transform.position).normalized * personMoving.maxSpeed;
+                    personMoving.dir += (desiredVel - meBody.velocity) * sameWeight;
+
+                }
+
+                //Add flock direction (will want to move in the same direction as neighbours)
+                Vector2 flockdesiredVel = (inRadius[i].GetComponent<Rigidbody2D>().velocity);
+                personMoving.dir += (flockdesiredVel - meBody.velocity) * flockWeight;
+            }
+
+            foreach (Transform t in nearBorders)
+            {
+                Vector2 desiredVel = (t.position - transform.position).normalized * personMoving.maxSpeed;
+                personMoving.dir -= (desiredVel - meBody.velocity) * borderWeight;
+            }
+
+            nearBorders.Clear();
+            inRadius.Clear();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
